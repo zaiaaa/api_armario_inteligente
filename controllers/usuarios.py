a7ca2ad9b5196be486ef1669018ca7927c3b7c27
zaiaaa <gustavozaia75@gmail.com>
2025-06-19 02:23:44 -0300

    implantação da pasta controller. Melhor organização do código.

---
+++ controllers/usuarios.py
@@ -0,0 +1,32 @@
+from connections.db import db
+from flask import jsonify, request
+
+usuarios = db.usuarios
+
+def listar_usuarios():
+    lista = list(usuarios.find({}, {"_id": 0}))
+    return jsonify(lista), 200
+
+def cadastrar_usuario():
+    dados = request.get_json()
+    if "UID" not in dados:
+        return jsonify({"erro": "UID é obrigatório"}), 400
+
+    if usuarios.find_one({"UID": dados["UID"]}):
+        return jsonify({"erro": "UID já cadastrado"}), 409
+
+    usuarios.insert_one(dados)
+    return jsonify({"mensagem": "Usuário cadastrado"}), 201
+
+def editar_usuario(uid):
+    dados = request.get_json()
+    res = usuarios.update_one({"UID": uid}, {"$set": dados})
+    if res.matched_count == 0:
+        return jsonify({"erro": "Usuário não encontrado"}), 404
+    return jsonify({"mensagem": "Atualizado com sucesso"}), 200
+
+def deletar_usuario(uid):
+    res = usuarios.delete_one({"UID": uid})
+    if res.deleted_count == 0:
+        return jsonify({"erro": "Usuário não encontrado"}), 404
+    return jsonify({"mensagem": "Deletado com sucesso"}), 200
--- routes/usuarios.py
+++ routes/usuarios.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from connections.db import db
+from controllers.usuarios import listar_usuarios, cadastrar_usuario, editar_usuario, deletar_usuario
 
 usuarios = db.usuarios
 
@@ -9,31 +10,15 @@ usuarios_bp = Blueprint("usuarios", __name__, url_prefix="/usuarios")
 @usuarios_bp.route("/", methods=["GET", "POST"])
 def usuarios_handler():
     if request.method == "GET":
-        lista = list(usuarios.find({}, {"_id": 0}))
-        return jsonify(lista), 200
+        return listar_usuarios()
 
     elif request.method == "POST":
-        dados = request.get_json()
-        if "UID" not in dados:
-            return jsonify({"erro": "UID é obrigatório"}), 400
-
-        if usuarios.find_one({"UID": dados["UID"]}):
-            return jsonify({"erro": "UID já cadastrado"}), 409
-
-        usuarios.insert_one(dados)
-        return jsonify({"mensagem": "Usuário cadastrado"}), 201
+        return cadastrar_usuario()
 
 @usuarios_bp.route("/<uid>", methods=["PUT", "DELETE"])
 def usuario_unico(uid):
     if request.method == "PUT":
-        dados = request.get_json()
-        res = usuarios.update_one({"UID": uid}, {"$set": dados})
-        if res.matched_count == 0:
-            return jsonify({"erro": "Usuário não encontrado"}), 404
-        return jsonify({"mensagem": "Atualizado com sucesso"}), 200
+        return editar_usuario(uid)
 
     elif request.method == "DELETE":
-        res = usuarios.delete_one({"UID": uid})
-        if res.deleted_count == 0:
-            return jsonify({"erro": "Usuário não encontrado"}), 404
-        return jsonify({"mensagem": "Deletado com sucesso"}), 200
+        return deletar_usuario(uid)
