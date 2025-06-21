6a66b32ac8a46d66f2d6d559cde3aacee5f9dfcd
zaiaaa <gustavozaia75@gmail.com>
2025-06-21 18:40:16 -0300

    controle de usuario feito pra abertura do armario

--- controllers/abertura.py
+++ controllers/abertura.py
@@ -3,12 +3,20 @@ from flask import jsonify, request
 from datetime import datetime
 
 abertura = db.abertura
+usuarios = db.usuarios
 
 def cadastrar_abertura():
-    dados = request.get_json()
+    dados = request.get_json()   
+    
     if "id_colaborador" not in dados:
         return jsonify({"erro": "Obrigatório id do colaborador."})
     hora_atual = datetime.now()
+
+    usuario = usuarios.find_one({"id_colaborador": dados["id_colaborador"]})
+    print(usuario)
+    if not usuario:
+        return jsonify({"mensagem": "Acesso negado."}), 403
+
 
     queryFinal = {
         "id_colaborador": dados["id_colaborador"],
@@ -22,6 +30,21 @@ def cadastrar_abertura():
         return jsonify({"mensagem": f"Erro -> {str(e)}"}), 400
 
 def listar_aberturas():
-    aberturas = list(abertura.find({}, {"_id": 0}))
-    return jsonify(aberturas)
+    aberturas = abertura.find({})
+
+    print(aberturas)
+
+    aberturas_formatadas = []
+
+    for ab in aberturas:
+        usuario = usuarios.find_one({"id_colaborador": ab["id_colaborador"]}, {"_id": 0, "nome": 1})
+        nome = usuario["nome"] if usuario else "Desconhecido"
+        
+        aberturas_formatadas.append({
+            "id_colaborador": ab["id_colaborador"],
+            "nome": nome,
+            "hora_abertura": ab["hora_abertura"]
+        })
+
+    return jsonify(aberturas_formatadas)
     
