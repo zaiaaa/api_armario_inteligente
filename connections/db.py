83c6b0e252948ef82c10d17133de2c8d7567fa93
zaiaaa <gustavozaia75@gmail.com>
2025-07-02 16:03:35 -0300

    protegendo uri e adicionando libs no requirements

--- connections/db.py
+++ connections/db.py
@@ -1,8 +1,8 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import os
 
-
-uri = "mongodb+srv://gustavozaia75:niFKgF8yAxAonvUm@armariointeligente.ejtutce.mongodb.net/?retryWrites=true&w=majority&appName=armarioInteligente"
+uri = os.environ.get("MONGO_URI")
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 db = client.armario_inteligente
