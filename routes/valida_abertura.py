c2c660e852df274d88d1efbcf1edfb3d6db071d3
zaiaaa <gustavozaia75@gmail.com>
2025-08-05 20:09:42 -0300

    nova rota de listar status

--- routes/valida_abertura.py
+++ routes/valida_abertura.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from connections.db import db
-from controllers.abertura import envia_formulario_retirada, listar_lockouts, envia_formulario_devolucao
+from controllers.abertura import lista_status, envia_formulario_retirada, listar_lockouts, envia_formulario_devolucao
 status_abertura = db.status_abertura
 
 valida_abertura_bp = Blueprint("status_abertura", __name__, url_prefix="/status_abertura")
@@ -9,6 +9,10 @@ valida_abertura_bp = Blueprint("status_abertura", __name__, url_prefix="/status_
 @valida_abertura_bp.route("/", methods=["GET"])
 def visualizar():
         return listar_lockouts()
+
+@valida_abertura_bp.route("/status", methods=["GET"])
+def status():
+        return lista_status()
 
 @valida_abertura_bp.route("/retirada", methods=["POST"])
 def retirada():
